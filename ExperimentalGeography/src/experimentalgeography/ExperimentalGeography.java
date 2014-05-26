8e31a158095ed99d338c3e1f44117bcc9e7b52c9
Daniel Johnson <danieljohnson2@verizon.net>
2014-05-26 08:56:15 -0400

    First cut: it places a bedrock block at the center of each chunk.

--- ExperimentalGeography/src/experimentalgeography/ExperimentalGeography.java
+++ ExperimentalGeography/src/experimentalgeography/ExperimentalGeography.java
@@ -11,30 +11,39 @@ import org.bukkit.event.world.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
+ * This is the main plugin class for the plugin; it listens to events.
  *
  * @author DanJ
  */
 public class ExperimentalGeography extends JavaPlugin implements Listener {
 
+    /**
+     * This method is called once per chunk, when the chunk is first
+     * initialized. It can add extra blocks to the chunk.
+     *
+     * @param chunk The chunk to populate.
+     */
+    private void populateChunk(Chunk chunk) {
+        int centerX = chunk.getX() * 16 + 8;
+        int centerZ = chunk.getZ() * 16 + 8;
+
+        int y = chunk.getWorld().getHighestBlockYAt(centerX, centerZ);
+
+        Block block = chunk.getBlock(8, y, 8);
+
+        block.setType(Material.BEDROCK);
+    }
+
     @Override
     public void onEnable() {
         super.onEnable();
-
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
     public void onChunkLoad(ChunkLoadEvent e) {
         if (e.isNewChunk()) {
-            Chunk chunk = e.getChunk();
-            int centerX = chunk.getX() * 16 + 8;
-            int centerZ = chunk.getZ() * 16 + 8;
-
-            int y = chunk.getWorld().getHighestBlockYAt(centerX, centerZ);
-
-            Block block = chunk.getBlock(8, y, 8);
-
-            block.setType(Material.BEDROCK);
+            populateChunk(e.getChunk());
         }
     }
 }
